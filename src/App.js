838c1235e9f632bc97dffb5e9db55c4f34e96222
Evan Cosmos <evanclark@live.ca>
2022-09-29 11:09:16 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/timeForm'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='WelcomeHead'>
+        Welcome to Spotitime
+        <div className='WelcomeSub'>
+          Enter in a time, and we'll make queue up music for that long
+        </div>
+      </div>
+
+    <timeEntry></timeEntry>
+
     </div>
   );
 }
